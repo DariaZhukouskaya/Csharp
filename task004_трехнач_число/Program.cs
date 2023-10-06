4dbd470f2e6c2ab283c67204513a167a9fb2d22c
Daria <dsobchik@gmail.com>
2023-10-06 12:21:20 +0300

    Решили задачу 7

---
+++ task004_трехнач_число/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 7. Напииште программу, которая принимает на входе трехзначное число, а на выходе показывет последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNamber1 = Convert.ToInt32(Console.ReadLine());
+int result = userNamber1 % 10;
+System.Console.WriteLine(result);
