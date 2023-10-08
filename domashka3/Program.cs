7afee78522507712a7de2ce12e7f4c078cd785b6
Daria <dsobchik@gmail.com>
2023-10-08 13:48:45 +0300

    Решили домашку

--- domashka3/Program.cs
+++ domashka3/Program.cs
@@ -6,7 +6,7 @@ number = 3 -> Число 3 нечётное
 number = 7 -> Число 7 нечётное
 */
 
-Console.WriteLine("Введите число: ");
+System.Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if (number % 2 == 0) 
