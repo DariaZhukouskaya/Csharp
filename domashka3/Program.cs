c1db838d95ed5698acc67c2471d0efbcc2b69648
Daria <dsobchik@gmail.com>
2023-10-08 13:54:55 +0300

    Решили домашку

--- domashka3/Program.cs
+++ domashka3/Program.cs
@@ -11,9 +11,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number % 2 == 0) 
 {
-    Console.WriteLine("Ваше число ЧЕТНОЕ ");
+    System.Console.WriteLine("Ваше число ЧЕТНОЕ ");
 }
 else 
 {
-    Console.WriteLine("Ваше число НЕЧЕТНОЕ ");
+    System.Console.WriteLine("Ваше число НЕЧЕТНОЕ ");
 }
