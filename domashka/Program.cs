0259109c8ad42010718be679ea6c0dabffd8ec84
Daria <dsobchik@gmail.com>
2023-10-09 11:43:30 +0300

    Решили задачу 1

--- domashka/Program.cs
+++ domashka/Program.cs
@@ -20,3 +20,19 @@ else
 {
     Console.WriteLine(number2 + " больше, чем " + number1);
 }
+
+/*
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = numberA % numberB;
+if (numberA % numberB == 0)
+{
+    System.Console.WriteLine("кратно");
+}
+else
+{
+    System.Console.WriteLine($"некратно, остаток {numberC}");
+}
+*/
