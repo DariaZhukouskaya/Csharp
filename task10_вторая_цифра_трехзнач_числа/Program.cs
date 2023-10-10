b08b26f69a185753d234d616b5f6fad27454ffd0
Daria <dsobchik@gmail.com>
2023-10-10 09:43:30 +0300

    Решили задачу 10

---
+++ task10_вторая_цифра_трехзнач_числа/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first = number / 10;
+int second = first % 10;
+
+System.Console.Write(second); 
