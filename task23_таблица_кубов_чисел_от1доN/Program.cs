0ae86e3f7d20a4cdfdbc4543e4d9142780fb6442
Daria <dsobchik@gmail.com>
2023-10-12 14:46:05 +0300

    Решили задачу 23

---
+++ task23_таблица_кубов_чисел_от1доN/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+23. Напишите программу, которая принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+N = 3 
+1
+8
+27
+
+N = 5
+1
+8
+27
+64
+125
+*/
+
+System.Console.WriteLine("Введи число: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= userNumber; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+} 
