290a795ab3cf1610061f78b11ae22d4585b91205
Daria <dsobchik@gmail.com>
2023-10-17 15:16:34 +0300

    Решили задачу 27

---
+++ task27_число_и_выдаёт_сумму_цифрвчисле/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumNumber(int a)
+{
+    int result = 0;
+    for (int i = a; i > 0; i /= 10)  // a делим на 10 каждый круг
+    {
+        result = result + i % 10;   // result = 0, 0 + i +% 10
+    }
+    return result;
+}
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int userRes = SumNumber(num);
+System.Console.WriteLine(userRes);
