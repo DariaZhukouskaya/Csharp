a34c9a94947eb63cb5e3bde8df26dd42a1b813f8
Daria <dsobchik@gmail.com>
2023-11-27 13:00:38 +0300

    Решили задачу 29

--- task29_массив_из_8элементов/Program.cs
+++ task29_массив_из_8элементов/Program.cs
@@ -5,26 +5,26 @@
 */
 
 
-int[] GetArray(int start, int end, int size)  //первая функция наполняет массив, вторая выводит на экран
+int[] GetArray(int start, int end, int size)  //запрашиваем у пользователя диапозон массива: первая функция наполняет массив, вторая выводит на экран
 {
     int[] array = new int[size]; //чтоб наполнить int[] array мы заходит в функцию
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(start, end+1);
+        array[i] = new Random().Next(start, end + 1); //начало и конец задается от пользователя
     }
     return array;
 }
 
 void PrintArray(int[] arr)
 {
-    Console.ForegroundColor=ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.Green;
     System.Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         System.Console.Write(arr[i]);   //[5, 7, 1, 4] - размер
         if (i < arr.Length - 1)         // 0, 1, 2, 3 - массив
-        System.Console.Write(", ");   
-    }                                 
+            System.Console.Write(", ");
+    }
     System.Console.Write("]");
     Console.ResetColor();
 }
@@ -37,4 +37,4 @@ System.Console.WriteLine("Задай размер массива:");
 int userSize = Convert.ToInt32(Console.ReadLine());
 
 int[] userArray = GetArray(userStart, userEnd, userSize);  // вызываем функцию
-PrintArray(userArray);  // печатаем функцию
+PrintArray(userArray);  // печатаем функцию (void метод)
