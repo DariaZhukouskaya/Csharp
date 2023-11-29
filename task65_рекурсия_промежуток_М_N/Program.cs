b0a74a91f99f31d395fdf76205d29af8902be62f
Daria <dsobchik@gmail.com>
2023-11-29 14:38:58 +0300

    Решили задачу 65

---
+++ task65_рекурсия_промежуток_М_N/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+65. Задайте значение М и N. 
+Напишите программу, которая выведет все натуральные исла в промежутке от М до N.
+*/
+
+System.Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void intN(int first, int last)
+{
+    if (last >= first)
+    {
+        System.Console.Write(first + " ");
+        intN(first +1, last);
+    }
+    else;
+        return;
+}
+intN(number1, number2);
