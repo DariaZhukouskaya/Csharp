cda2d3803c0f6f98e118f7fd7991e6296bf116c0
Daria <dsobchik@gmail.com>
2023-11-29 14:29:06 +0300

    Решили задачу 63

---
+++ task63_рекурсия_числа_до_N/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+63. Задайте значение N. Напишите прорграмму, которая выведет все натуральные числа от 1 до N.
+*/
+
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void intN(int number, int current = 1)
+{
+    if (number < current) return;
+    else 
+    {
+        System.Console.Write(current + " ");
+        intN(number, current + 1);
+    }
+    return;
+}
+intN(number);
