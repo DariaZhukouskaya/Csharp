5309b0c5174cf9755514b01eb4eaa526e8d53eb1
Daria <dsobchik@gmail.com>
2023-11-30 22:17:46 +0300

    Pешили задачу 23

--- task23_таблица_кубов_чисел_от1доN/obj/Debug/net6.0/task23_таблица_кубов_чисел_от1доN.AssemblyInfo.cs
+++ task23_таблица_кубов_чисел_от1доN/obj/Debug/net6.0/task23_таблица_кубов_чисел_от1доN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task23_таблица_кубов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1cf9d96e687629ed38b6a8ae36b8958f796585")]
 [assembly: System.Reflection.AssemblyProductAttribute("task23_таблица_кубов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task23_таблица_кубов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
