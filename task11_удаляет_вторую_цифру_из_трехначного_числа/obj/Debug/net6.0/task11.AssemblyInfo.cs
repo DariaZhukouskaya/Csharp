b4c253eef6fc0689af85562e587272d9249609ae
Daria <dsobchik@gmail.com>
2023-11-30 22:09:29 +0300

    Pешили задачу 11

--- task11_удаляет_вторую_цифру_из_трехначного_числа/obj/Debug/net6.0/task11.AssemblyInfo.cs
+++ task11_удаляет_вторую_цифру_из_трехначного_числа/obj/Debug/net6.0/task11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3d6385544a0c13ed5f398a9aec7d6d63b72677")]
 [assembly: System.Reflection.AssemblyProductAttribute("task11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
