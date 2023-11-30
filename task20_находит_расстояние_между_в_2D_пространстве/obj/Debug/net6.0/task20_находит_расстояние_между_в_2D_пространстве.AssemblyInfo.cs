9c05c5d7a818046a1c4cd9c162ad23f45a11a81f
Daria <dsobchik@gmail.com>
2023-11-30 22:15:34 +0300

    Pешили задачу 20

--- task20_находит_расстояние_между_в_2D_пространстве/obj/Debug/net6.0/task20_находит_расстояние_между_в_2D_пространстве.AssemblyInfo.cs
+++ task20_находит_расстояние_между_в_2D_пространстве/obj/Debug/net6.0/task20_находит_расстояние_между_в_2D_пространстве.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task20_находит_расстояние_между_в_2D_пространстве")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aeb326dd4d03c9cf743bfd6033d79dd2e2a935f")]
 [assembly: System.Reflection.AssemblyProductAttribute("task20_находит_расстояние_между_в_2D_пространстве")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task20_находит_расстояние_между_в_2D_пространстве")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
