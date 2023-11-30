765dd9dde6d4b04661db0190fb1f3625f5ed96bd
Daria <dsobchik@gmail.com>
2023-11-30 22:12:10 +0300

    Pешили задачу 15

--- task15_день_недели_вых_или_нет/obj/Debug/net6.0/task15_день_недели_вых_или_нет.AssemblyInfo.cs
+++ task15_день_недели_вых_или_нет/obj/Debug/net6.0/task15_день_недели_вых_или_нет.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task15_день_недели_вых_или_нет")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a921054be2f8959958c2e7ee9fd83261d0b9fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("task15_день_недели_вых_или_нет")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task15_день_недели_вых_или_нет")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
