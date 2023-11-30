bc1cf9d96e687629ed38b6a8ae36b8958f796585
Daria <dsobchik@gmail.com>
2023-11-30 22:17:20 +0300

    Pешили задачу 22

--- task22_таблицу_квадратов_чисел_от1доN/obj/Debug/net6.0/task22_таблицу_квадратов_чисел_от1доN.AssemblyInfo.cs
+++ task22_таблицу_квадратов_чисел_от1доN/obj/Debug/net6.0/task22_таблицу_квадратов_чисел_от1доN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task22_таблицу_квадратов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ca2855206a62d8253a6dc4722e16f42c89a119")]
 [assembly: System.Reflection.AssemblyProductAttribute("task22_таблицу_квадратов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task22_таблицу_квадратов_чисел_от1доN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
