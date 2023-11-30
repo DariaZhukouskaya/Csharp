43c092c990887b8446849392811e83104b39ce1f
Daria <dsobchik@gmail.com>
2023-11-30 22:02:53 +0300

    Pешили задачу 1

--- task02_большее_и_меньшее_числа/obj/Debug/net6.0/task02_большее_и_меньшее_числа.AssemblyInfo.cs
+++ task02_большее_и_меньшее_числа/obj/Debug/net6.0/task02_большее_и_меньшее_числа.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task02_большее_и_меньшее_числа")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c4faa05aef13f220265ea3ef8668fd2decdad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8d85d53ab5a7654b2fcc3cb474994014b854ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("task02_большее_и_меньшее_числа")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task02_большее_и_меньшее_числа")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
