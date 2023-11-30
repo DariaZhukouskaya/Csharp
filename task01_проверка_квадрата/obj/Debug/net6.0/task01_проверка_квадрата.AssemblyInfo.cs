1f8d85d53ab5a7654b2fcc3cb474994014b854ac
Daria <dsobchik@gmail.com>
2023-11-30 22:02:11 +0300

    Pешили задачу 1

--- task01_проверка_квадрата/obj/Debug/net6.0/task01_проверка_квадрата.AssemblyInfo.cs
+++ task01_проверка_квадрата/obj/Debug/net6.0/task01_проверка_квадрата.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task01_проверка_квадрата")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c1e7a5c4e61c7df36871bca21ba4306c236a71")]
 [assembly: System.Reflection.AssemblyProductAttribute("task01_проверка_квадрата")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task01_проверка_квадрата")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
