103bb8c74ca8c9070ed2c38ec5d69d6b4c24d673
Daria <dsobchik@gmail.com>
2023-11-30 22:18:49 +0300

    Pешили задачу 24

--- task24_сумму_чисел_от1доA/obj/Debug/net6.0/task24_сумму_чисел_от1доA.AssemblyInfo.cs
+++ task24_сумму_чисел_от1доA/obj/Debug/net6.0/task24_сумму_чисел_от1доA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task24_сумму_чисел_от1доA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5309b0c5174cf9755514b01eb4eaa526e8d53eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("task24_сумму_чисел_от1доA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task24_сумму_чисел_от1доA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
